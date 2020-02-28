35f064c0ed2f1f6d6e8dbdc8a294abd219d6ad76
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-02-28 17:12:51 -0300

    Create scientist.py

---
+++ scientist.py
@@ -0,0 +1,23 @@
+
+import pygame
+
+#Building the hero of our game
+
+class Scientist():
+    def __init__(self,screen):
+
+        self.screen=screen
+
+        #loading the scientist
+        self.image=pygame.image.load("Covid_Scientist.bmp")
+        self.rect=self.image.get_rect()
+        self.screen_rect=screen.get_rect()
+
+        #start each new scientist in the bottom center of the screen
+        self.rect.centerx=self.screen_rect.centerx
+        self.rect.centery=self.screen_rect.centery
+
+    def blitme(self):
+        #drawn the scientist at its current location
+        self.screen.blit(self.image,self.rect)
+
