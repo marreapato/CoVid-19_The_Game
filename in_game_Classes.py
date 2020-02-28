0e8fbcd7e722e787b2549101c032501fb28c3d00
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-02-28 16:28:45 -0300

    Create in_game_Classes.py

---
+++ in_game_Classes.py
@@ -0,0 +1,7 @@
+class Settings():
+    #Class made with the purpose of storing all the settings of the game
+    def __init__(self):
+        
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
