74e38366c8b87ec13f17879f72ee8bd63913b5c9
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-02-28 15:57:49 -0300

    Create Covid19_The_Game.py

---
+++ Covid19_The_Game.py
@@ -0,0 +1,30 @@
+import sys
+
+import pygame
+
+#the funtion is suppose to initialize the game, create a screen object and watch for the events
+
+def run_game():
+
+    pygame.init()
+
+    screen=pygame.display.set_mode((1200,800))#uses a tuple to set the dimensions
+
+    pygame.display.set_caption("Covid19 The Game")
+
+    #main loop for the game
+
+    while True:
+
+        #watching for the events
+        for event in pygame.event.get():
+
+            if event.type==pygame.QUIT:
+
+                sys.exit()
+
+            #Drawn the mostly recent screen
+            pygame.display.flip()
+
+
+run_game()
