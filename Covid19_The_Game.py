60f086198f089919e1c37b0cfe88e6b3c62b4c8f
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-02-28 17:34:25 -0300

    Update Covid19_The_Game.py

--- Covid19_The_Game.py
+++ Covid19_The_Game.py
@@ -4,32 +4,40 @@ import pygame
 
 from settings import Settings
 
-#the funtion is supposed to initialize the game, create a screen object and watch for the events
+from scientist import Scientist
+
+
+# the funtion is supposed to initialize the game, create a screen object and watch for the events
 
 def run_game():
-
     pygame.init()
 
-    ai_settings=Settings()
+    ai_settings = Settings()
 
-    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))#uses a tuple to set the dimensions
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))  # uses a tuple to set the dimensions
 
     pygame.display.set_caption("Covid19 The Game")
 
-    #main loop for the game
+    #creting the scientist
+
+    scientist=Scientist(screen)
+
+    # main loop for the game
 
     while True:
 
-        #watching for the events
+        # watching for the events
         for event in pygame.event.get():
 
-            if event.type==pygame.QUIT:
-
+            if event.type == pygame.QUIT:
                 sys.exit()
 
-            #Drawn the mostly recent screen
-            
+            # Drawn the mostly recent screen
+
             screen.fill(ai_settings.bg_color)
+
+            scientist.blitme()#drawns the scientist
 
             pygame.display.flip()
 
