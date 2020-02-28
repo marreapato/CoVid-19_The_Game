ec53d081830c99b3299ab26727feebb4f788defe
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-02-28 16:33:57 -0300

    Update Covid19_The_Game.py

--- Covid19_The_Game.py
+++ Covid19_The_Game.py
@@ -2,13 +2,17 @@ import sys
 
 import pygame
 
+from in_game_Classes import Settings
+
 #the funtion is supposed to initialize the game, create a screen object and watch for the events
 
 def run_game():
 
     pygame.init()
 
-    screen=pygame.display.set_mode((1200,800))#uses a tuple to set the dimensions
+    ai_settings=Settings()
+
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))#uses a tuple to set the dimensions
 
     pygame.display.set_caption("Covid19 The Game")
 
@@ -24,6 +28,9 @@ def run_game():
                 sys.exit()
 
             #Drawn the mostly recent screen
+            
+            screen.fill(ai_settings.bg_color)
+
             pygame.display.flip()
 
 
