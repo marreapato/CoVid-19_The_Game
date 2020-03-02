78d3a17422e09bd57ac7125872f734294ccd9670
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-03-02 08:17:29 -0300

    Update events.py

--- events.py
+++ events.py
@@ -2,8 +2,20 @@ import sys
 
 import pygame
 
+#Function designed to run the in-game events
+
 def events():
 
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings,screen,scientist):
+
+    screen.fill(ai_settings.bg_color)
+    #ai_settings stands for the Settings() class' instance
+    scientist.blitme()
+
+    pygame.display.flip()
+
+#bruno.dmasceno@gmail.com
