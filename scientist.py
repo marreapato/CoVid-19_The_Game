ad0c49e785f6fa148c83a1f08d92d77f93f96636
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-03-03 13:58:18 -0300

    Update scientist.py

--- scientist.py
+++ scientist.py
@@ -4,6 +4,7 @@ import pygame
 #Building the hero of our game
 
 class Scientist():
+
     def __init__(self,screen):
 
         self.screen=screen
@@ -17,7 +18,18 @@ class Scientist():
         self.image_rect.centerx=self.screen_rect.centerx
         self.image_rect.bottom=self.screen_rect.bottom
 
+        self.moving_right=False
+        self.moving_left=False
     def blitme(self):
         #drawn the scientist at its current location
         self.screen.blit(self.image,self.image_rect)
 
+    def update(self):#this function updates th position of the scientist
+
+        if self.moving_right==True:
+
+            self.image_rect.centerx+=1
+
+        if self.moving_left==True:
+#it's better to use an if in both cases, because the user can press two buttons at the same time
+            self.image_rect.centerx-=1
