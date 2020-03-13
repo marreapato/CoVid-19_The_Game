2d863dbd5a95f8f759f153e674fa5c7ad93fee6e
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-03-12 21:16:45 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,3 +8,11 @@ class Settings():
         self.screen_height=600
         self.bg_color=(100,0,230)
         self.scientist_speed=1.5
+
+
+        #Creating the cure
+
+        self.cure_speed_factor=1
+        self.cure_width=3
+        self.cure_height=15
+        self.cure_color=(60,60,60)
