e168e855afe1a679dc8a58bf77d1aaef9466f78f
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-03-17 22:15:01 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -15,4 +15,4 @@ class Settings():
         self.cure_speed_factor=1
         self.cure_width=3
         self.cure_height=15
-        self.cure_color=(60,60,60)
+        self.cure_color=(0,100,50)#green cure
