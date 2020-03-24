f2882df294480a48ffe3bddb10deb27a85c4e0da
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-03-23 21:12:52 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings():
 
         self.screen_width=1000
         self.screen_height=600
-        self.bg_color=(100,0,230)
+        self.bg_color=(100,10,0)
         self.scientist_speed=1.5
 
 
