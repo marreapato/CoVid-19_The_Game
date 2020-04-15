a065ef0048c14db0097790f6018eacca3746d0b7
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-04-15 17:37:21 -0300

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -3,8 +3,10 @@ class Games_stats():
     def __init__(self,ai_settings):
         self.ai_settings=ai_settings
         self.reset_stats()
-        self.game_active=True
+        self.game_active=False
+
 
 
     def reset_stats(self):
         self.scientist_left=self.ai_settings.scientist_limit
+        self.score=0
